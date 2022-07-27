ecc1b644b15ca7aa3f6c34ed262bc89a92d3a708
Danilo Carvalho <danilocarvalhosp@gmail.com>
2022-07-27 19:10:56 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank_ADM/Program.cs
@@ -0,0 +1,30 @@
+﻿using ByteBank_ADM.Funcionarios;
+using ByteBank_ADM.Utilitario;
+
+GerenciadorBonificacoes gerenciador = new GerenciadorBonificacoes();
+
+Console.WriteLine("Boas Vindas ao ByteBank Administração.");
+
+Funcionario pedro = new Funcionario();
+pedro.Nome = "Pedro";
+pedro.Cpf = "10252462425";
+pedro.Salario = 2000;
+
+//Console.WriteLine("Nome: " + pedro.Nome);
+//Console.WriteLine("CPF: " + pedro.Cpf);
+//Console.WriteLine("Salário: " + pedro.Salario);
+
+Diretor paula = new Diretor();
+paula.Nome = "Paula";
+paula.Cpf = "258746325894";
+paula.Salario = 5000;
+
+Console.WriteLine("Bonificação: " + pedro.getBonificacao());
+Console.WriteLine("Bonificação: " + paula.getBonificacao());
+
+gerenciador.Registrar(pedro);
+gerenciador.Registrar(paula);
+
+Console.WriteLine("Total de Bonificações: " + gerenciador.getBonificacao());
+
+Console.ReadKey();
---
+++ ByteBank_ADM/Utilitario/GerenciadorBonificacoes.cs
@@ -0,0 +1,26 @@
+﻿using ByteBank_ADM.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank_ADM.Utilitario
+{
+    public class GerenciadorBonificacoes
+    {
+        private double TotalBonificacao;
+
+        public void Registrar(Funcionario funcionario) {
+            this.TotalBonificacao += funcionario.getBonificacao();
+        }
+
+        public void Registrar(Diretor diretor) {
+            this.TotalBonificacao += diretor.getBonificacao();
+        }
+
+        public double getBonificacao() {
+            return this.TotalBonificacao;
+        }
+    }
+}
