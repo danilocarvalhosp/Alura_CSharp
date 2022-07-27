36c2bbac00b9864642172817a8801fffd8c247bb
Danilo Carvalho <danilocarvalhosp@gmail.com>
2022-07-27 19:38:11 -0300

    A palavra reservada base

--- ByteBank_ADM/Funcionarios/Diretor.cs
+++ ByteBank_ADM/Funcionarios/Diretor.cs
@@ -9,7 +9,7 @@ namespace ByteBank_ADM.Funcionarios
     public class Diretor : Funcionario
     {
         public override double getBonificacao() {
-            return Salario;
+            return Salario + base.getBonificacao();
         }
     }
 }
