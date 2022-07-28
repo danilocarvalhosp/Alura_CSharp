b154e5c66b1d8d5fd97ed8d0a2e7879b7ecedb67
Danilo Carvalho <danilocarvalhosp@gmail.com>
2022-07-28 20:35:58 -0300

    Primeiro Array

---
+++ bytebank_ATENDIMENTO/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Boas Vindas ao ByteBank - Atendimento.");
+
+int [] idades = new int [5];
+
+TestaArrayInt();
+
+void TestaArrayInt()
+{
+    idades [0] = 30;
+    idades [1] = 40;
+    idades [2] = 17;
+    idades [3] = 21;
+    idades [4] = 18;
+
+    Console.WriteLine($"Tamanho do Array {idades.Length}");
+
+    int acumulador = 0;
+
+    for (int i = 0; i < idades.Length; i++)
+    {
+        int idade = idades [i];
+        Console.WriteLine($"Índice [{i}] = {idades [i]}");
+        acumulador += idades [i];
+    }
+
+    int media = acumulador / idades.Length;
+    Console.WriteLine($"Média de idades = {media}");
+}
+
+
