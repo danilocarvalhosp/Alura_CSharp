29899e6ed53139d77b859cd5e0e6fd8bebae734f
Danilo Carvalho <danilocarvalhosp@gmail.com>
2022-07-28 19:28:05 -0300

    Criando nossa exceção

--- ByteBank/ContaCorrente.cs
+++ ByteBank/ContaCorrente.cs
@@ -71,15 +71,14 @@ namespace ByteBank
         }
 
 
-        public bool Sacar(double valor)
+        public void Sacar(double valor)
         {
             if (_saldo < valor)
             {
-                return false;
+                throw new SaldoInsuficienteException("Saldo insuficiente para saque no valor de " + valor);
             }
 
             _saldo -= valor;
-            return true;
         }
 
         public void Depositar(double valor)
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -8,7 +8,7 @@ namespace ByteBank
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string [] args)
         {
             try
             {
@@ -17,11 +17,17 @@ namespace ByteBank
                 Console.WriteLine(conta.Saldo);
                 conta.Sacar(500);
                 Console.WriteLine(conta.Saldo);
-            } catch (ArgumentException ex)
+            }
+            catch (ArgumentException ex)
             {
                 Console.WriteLine("Erro no parâmetro: " + ex.ParamName);
                 Console.WriteLine("Ocorreu um erro do tipo ArgumentException.");
                 Console.WriteLine(ex.Message);
+            }
+            catch (SaldoInsuficienteException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Exceção do tipo SaldoInsuficienteException.");
             }
 
             // Console.WriteLine(ContaCorrente.TaxaOperacao);
