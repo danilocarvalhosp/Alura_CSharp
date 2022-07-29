78ff6e0f881f9db077e2825c77d1d92b6d15accc
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-07-29 10:15:55 -0300

    Classe lista de contas

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,4 +1,5 @@
 ﻿using bytebank.Modelos.Conta;
+using bytebank_ATENDIMENTO.bytebank.Util;
 
 Console.WriteLine("Boas Vindas ao ByteBank - Atendimento.");
 
@@ -89,18 +90,13 @@ void TestaMediana(Array array)
 
 void TestaArrayDeContasCorrentes()
 {
-    ContaCorrente [] listaDeContas = new ContaCorrente []
-    {
-        new ContaCorrente(874, "5679787-A"),
-        new ContaCorrente(874, "4456668-B"),
-        new ContaCorrente(874, "7781438-C")
-    };
-
-    for (int i = 0; i < listaDeContas.Length; i++)
-    {
-        ContaCorrente contaAtual = listaDeContas [i];
-        Console.WriteLine($"Índice {i} - Conta: {contaAtual.Conta}");
-    }
+    ListaDeContasCorrentes listaDeContas = new ListaDeContasCorrentes();
+    listaDeContas.Adicionar(new ContaCorrente(874, "5679787-A"));
+    listaDeContas.Adicionar(new ContaCorrente(874, "4456668-B"));
+    listaDeContas.Adicionar(new ContaCorrente(874, "7781438-C"));
+    listaDeContas.Adicionar(new ContaCorrente(874, "7781438-C"));
+    listaDeContas.Adicionar(new ContaCorrente(874, "7781438-C"));
+    listaDeContas.Adicionar(new ContaCorrente(874, "7781438-C"));
 }
 
 TestaArrayDeContasCorrentes();
