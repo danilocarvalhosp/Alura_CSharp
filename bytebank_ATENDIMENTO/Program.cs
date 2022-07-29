540ef5825b9a3b084b106737c54d37033a0f0bcb
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-07-29 09:16:13 -0300

    A classe Array

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -3,7 +3,7 @@
 int [] idades = new int [5];
 
 // TestaArrayInt();
-TestaBuscarPalavra();
+// TestaBuscarPalavra();
 
 void TestaArrayInt()
 {
@@ -51,4 +51,28 @@ void TestaBuscarPalavra()
     }
 }
 
+Array amostra = Array.CreateInstance(typeof(double), 5);
+amostra.SetValue(5.9, 0);
+amostra.SetValue(1.8, 1);
+amostra.SetValue(7.1, 2);
+amostra.SetValue(10, 3);
+amostra.SetValue(6.9, 4);
 
+TestaMediana(amostra);
+
+void TestaMediana(Array array)
+{
+    if (array == null || array.Length == 0)
+    {
+        Console.WriteLine("Array para cálculo da mediana está vazio ou nulo.");
+    }
+
+    double [] numerosOrdenados = (double []) array.Clone();
+    Array.Sort(numerosOrdenados);
+
+    int tamanho = numerosOrdenados.Length;
+    int meio = tamanho / 2;
+    double mediana = (tamanho % 2 != 0) ? numerosOrdenados [meio] : (numerosOrdenados[meio] + numerosOrdenados [meio -1]) / 2;
+
+    Console.WriteLine($"Com base na amostra, a mediana = {mediana}.");
+}
